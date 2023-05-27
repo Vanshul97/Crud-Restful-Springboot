4312c19e87e0f98f526f757e57006d9ff0be667a
Vanshul Suneja <72281663+Vanshul97@users.noreply.github.com>
2023-05-28 02:06:04 +0530

    Add files via upload

---
+++ springboot-crud-restful-webservices-master/src/main/java/net/javaguides/springboot/SpringbootCrudRestfulWebservicesApplication.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(scanBasePackages={"net.javaguides.springboot"})
+public class SpringbootCrudRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulWebservicesApplication.class, args);
+	}
+
+}
